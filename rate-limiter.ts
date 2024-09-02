a69e4d476c3eec051bde30d45ec4f790f96a1506
harpreetsingh22 <harpreet206866@gmail.com>
2024-09-03 01:04:15 +0530

    Code refactored

--- rate-limiter.ts
+++ rate-limiter.ts
@@ -1,6 +1,5 @@
 class RateLimiter {
-
-    private static rateLimiterInstance: RateLimiter;
+    private static rateLimiterInstance: RateLimiter | undefined;
     private windowSizeInMilliSeconds: number;
     private noOfRequestsAllowed: number;
     private requestInfoMap: Map<string, number[]>;
@@ -12,42 +11,34 @@ class RateLimiter {
     }
 
     public static getInstance(noOfRequestsAllowed: number, windowSizeInMilliSecond: number): RateLimiter {
-        if (this.rateLimiterInstance === null) {
+        if (!this.rateLimiterInstance) {
             this.rateLimiterInstance = new RateLimiter(noOfRequestsAllowed, windowSizeInMilliSecond);
         }
-
         return this.rateLimiterInstance;
     }
 
-
     public isRequestAllowed(requestId: string): boolean {
-        const requestInfo = this.requestInfoMap?.get(requestId);
         const currentTime = Date.now();
+        let requestInfo = this.requestInfoMap.get(requestId) || [];
 
-        if (requestInfo) {
-            const totalRequests = this.requestInfoMap.get(requestId);
-            const requestsInWindow = totalRequests?.filter((timestamp) => {
-                return currentTime - timestamp < this.windowSizeInMilliSeconds;
-            }) || [];
+        // Filter out timestamps outside the window size
+        requestInfo = requestInfo.filter((timestamp) => currentTime - timestamp < this.windowSizeInMilliSeconds);
 
-            if (requestsInWindow?.length >= this.noOfRequestsAllowed) {
-                return false;
-            }
-            else {
-                this.requestInfoMap?.set(requestId, [currentTime]);
-                return true;
-            }
-
-        }
-        else {
-            this.requestInfoMap?.set(requestId, [currentTime]);
-            return true;
+        // Check if the number of requests is within the allowed limit
+        if (requestInfo.length >= this.noOfRequestsAllowed) {
+            return false;
         }
 
+        // Add current request timestamp and update map
+        requestInfo.push(currentTime);
+        this.requestInfoMap.set(requestId, requestInfo);
+
+        return true;
     }
 }
 
-const rateLimiter = RateLimiter.getInstance(1,10000);
+// Usage example
+const rateLimiter = RateLimiter.getInstance(1, 10000);
 
 setInterval(() => {
     const clientId = 'client_1';
